dbe7ee3455ee21af8c7202a6a95ec44cef3e9a58
Omar-Belghaouti <bel_omar18@yahoo.com>
2020-04-06 20:06:31 +0200

    Adding a navigation page

--- FirstCrossXamarin/FirstCrossXamarin/Views/LoginPage.xaml.cs
+++ FirstCrossXamarin/FirstCrossXamarin/Views/LoginPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using FirstCrossXamarin.Models;
+using FirstCrossXamarin.Views.Menu;
 
 namespace FirstCrossXamarin.Views
 {
@@ -19,6 +20,7 @@ namespace FirstCrossXamarin.Views
             Init();
         }
 
+        [Obsolete]
         void Init()
         {
             BackgroundColor = Constants.BackgroundColor;
@@ -32,16 +34,27 @@ namespace FirstCrossXamarin.Views
             Entry_Password.Completed += (s, e) => SignInProcedure(s, e);
         }
 
+        [Obsolete]
         async void SignInProcedure(object sender, EventArgs e)
         {
             User user = new User(Entry_Username.Text, Entry_Password.Text);
             if (user.CheckInformation())
             {
                 DisplayAlert("Login", "You have logged in successfuly!", "Okay");
-                var result = await App.RestService.Login(user);
+                //var result = await App.RestService.Login(user);
+                var result = new Token();
                 if (result.access_token != null)
                 {
-                    App.UserDatabase.SaveUser(user);
+                    //App.UserDatabase.SaveUser(user);
+                    //App.TokenDatabase.SaveToken(result);
+                    if (Device.OS == TargetPlatform.Android)
+                    {
+                        Application.Current.MainPage = new NavigationPage(new Dashboard());
+                    }
+                    else if (Device.OS == TargetPlatform.iOS)
+                    {
+                        await Navigation.PushModalAsync(new NavigationPage(new Dashboard()));
+                    }
                 }
             }
             else
